a25a09245ac8b124d8572ec4062b7a3405e0c081
alexsouzaac <alexsouza.acre@gmail.com>
2025-12-06 16:54:36 -0500

    att v13

--- portfolio.ts
+++ portfolio.ts
@@ -20,7 +20,7 @@ export const partners = [
     description: "Referência em Saúde Mental e cuidado integral. Doutor em Ciências e Mestre em Medicina, une rigor científico com atendimento humanizado. Especialista no tratamento de ansiedade, depressão e transtornos mentais. \n\nCRM/AC 749 | RQE 201, 1990, 1256.",
     image: "https://raw.githubusercontent.com/alexsouzaac/site-alex-novo/main/foto-perfil/marcelus.jpg", 
     // Link atualizado abaixo com a nova frase (consulta)
-    link: "https://wa.me/5568999736712?text=Ol%C3%A1%2C%20vim%20pela%20indica%C3%A7%C3%A3o%20do%20Alex%20Souza%2C%20e%20estou%20interessado%20em%20consulta%20m%C3%A9dica%20psiqui%C3%A1trica.",
+    link: "https://wa.me/68992260113?text=Ol%C3%A1%2C%20vim%20pela%20indica%C3%A7%C3%A3o%20do%20Alex%20Souza%2C%20e%20estou%20interessado%20em%20uma consulta%20m%C3%A9dica%20psiqui%C3%A1trica.",
     buttonText: "Agendar Consulta",
     instagram: "https://instagram.com/drmarcelusnegreiros",
     mapLink: "https://maps.google.com/?q=Rua+Jasmim%2C+04.+Jardim+Tropical.+Rio+Branco%2C+Acre"
@@ -33,7 +33,7 @@ export const partners = [
     link: "https://wa.me/5568999052475?text=Ol%C3%A1%2C%20vim%20pela%20indica%C3%A7%C3%A3o%20do%20Alex%20Souza%2C%20e%20estou%20interessado%20em%20avalia%C3%A7%C3%A3o%20neuropsicol%C3%B3gica.",
     buttonText: "Agendar Consulta",
     instagram: "https://instagram.com/cafecompsico",
-    mapLink: "https://maps.app.goo.gl/JPH48ap9F3y8jF7r8"
+    mapLink: "https://www.google.com/maps/place/Caf%C3%A9+com+Psico/@-9.9734414,-67.8311671,17z/data=!4m14!1m7!3m6!1s0x917f8d6f4fc3b5fd:0xb5af0845b2e0a5d5!2sCaf%C3%A9+com+Psico!8m2!3d-9.9734414!4d-67.8285922!16s%2Fg%2F11vhz0srz_!3m5!1s0x917f8d6f4fc3b5fd:0xb5af0845b2e0a5d5!8m2!3d-9.9734414!4d-67.8285922!16s%2Fg%2F11vhz0srz_?entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D"
   }
 ];
 
